6aef5788f0e67294e9c419f1060913f8b3d20e38
Masvi <jeanmasvi@gmail.com>
2021-05-28 10:44:15 -0300

    feat: show only favorites characters

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,9 +4,6 @@
     <div class="home__search-section">
       <base-search />
     </div>
-    <div>
-      {{ currentFavorites }}
-    </div>
     <div class="home__menu">
       <div class="home__menu results">
         Econtrados 20 heróis
@@ -19,7 +16,10 @@
           >
           Ordernar por nome de A/Z
         </div>
-        <div>
+        <div
+
+          @click="showOnlyFavorites()" 
+        >
           <img
             src="../assets/favorito_01.svg"
             alt="marvel"
@@ -30,13 +30,26 @@
     </div>
     <div class="home__list">
       <base-card-item
-        v-for="item of characters"
+        v-for="item of currentList"
         :key="item.id"
         :name="item.name"
         :img-url="item.thumbnail.path"
         :extension="item.thumbnail.extension"
         @onClick="setAsFavorite(item)"
       /> 
+    </div>
+    <div 
+      v-if="currentList.length === 0"
+      class="home__list--no-results"
+    >
+      Você não possuí favoritos
+    </div>
+    <div
+      v-if="onlyFavorites" 
+      class="home__back"
+      @click="showMainList()"
+    >
+      <a>Voltar</a>
     </div>
   </div>
 </template>
@@ -54,6 +67,8 @@ export default {
   data() {
     return {
       characters: [],
+      currentList: [],
+      onlyFavorites: false
     }
   },
   computed: {
@@ -68,16 +83,25 @@ export default {
     findCharacters() {
       marvelService.getCharacters().then(({ data }) => {
         this.characters = data.data.results;
+        this.currentList = this.characters;
       });
     },
-    setAsFavorite({ id }) {
-      const isFavorite = this.currentFavorites.find((item) => id === item);
+    setAsFavorite(character) {
+      const favorite = this.currentFavorites.find((item) => character.id === item.id);
   
-      if (!(isFavorite)) {
-        return this.$store.dispatch("setFavorite", id); 
+      if (!(favorite)) {
+        return this.$store.dispatch("setFavorite", character); 
       } 
 
-      this.$store.dispatch("unsetFavorite", id); 
+      this.$store.dispatch("unsetFavorite", character); 
+    },
+    showOnlyFavorites() {
+      this.onlyFavorites = true;
+      this.currentList = this.currentFavorites;
+    },
+    showMainList() {
+      this.onlyFavorites = false;
+      this.currentList = this.characters;
     }
   }
 }
@@ -107,13 +131,13 @@ export default {
     margin: 1.5rem 0;
     width: 100%;
   
-    & .results { 
+    &.results { 
       font-size: 1.2rem;
       font-weight: bold;
       color: $primary-gray;
     }
 
-    & .options {
+    &.options {
       font-size: 1rem;
       color: $secondary-red;
     }
@@ -134,6 +158,20 @@ export default {
     justify-content:center;
     flex-direction: row;
     flex-wrap: wrap;
+
+    &--no-results {
+      display: flex;
+      align-items: center;
+      flex-direction: column;
+      font-size: 1rem;
+    }
+  }
+
+  &__back {
+    padding: 2rem;
+    font-size: 1.3rem;
+    color: $secondary-red;
+    cursor: pointer;
   }
 }
 
