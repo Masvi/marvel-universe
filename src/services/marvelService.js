a64cdd8b9ef89de78fe44a977be7e255952aa9ce
masvi <jeanmasvi@gmail.com>
2022-07-09 06:42:10 -0300

    remove env

--- src/services/marvelService.js
+++ src/services/marvelService.js
@@ -1,20 +1,27 @@
-import api from '../api/api';
-import auth from '../api/auth';
+import api from "../api/api";
+import auth from "../api/auth";
 
 const public_key = auth.getPublicKey();
 
 export default {
-
-  getCharacters({offset}) {
-    return api.get(`/characters?&offset=${offset}&ts=${auth.getTimeStamp()}&apikey=${public_key}&hash=${auth.getHashMd5()}`);
+  getCharacters({ offset }) {
+    return api.get(
+      `/characters?&offset=${offset}&ts=${auth.getTimeStamp()}&apikey=${public_key}&hash=${auth.getHashMd5()}`
+    );
   },
   getCharacterById(id) {
-    return api.get(`/characters/${id}?ts=${auth.getTimeStamp()}&apikey=${public_key}&hash=${auth.getHashMd5()}`);
+    return api.get(
+      `/characters/${id}?ts=${auth.getTimeStamp()}&apikey=${public_key}&hash=${auth.getHashMd5()}`
+    );
   },
   getCharacterByName(name) {
-    return api.get(`/characters?name=${name}&ts=${auth.getTimeStamp()}&apikey=${public_key}&hash=${auth.getHashMd5()}`);
+    return api.get(
+      `/characters?name=${name}&ts=${auth.getTimeStamp()}&apikey=${public_key}&hash=${auth.getHashMd5()}`
+    );
   },
   getComicById(id) {
-    return api.get(`/comics/${id}?ts=${auth.getTimeStamp()}&apikey=${public_key}&hash=${auth.getHashMd5()}`);
-  }
-}
+    return api.get(
+      `/comics/${id}?ts=${auth.getTimeStamp()}&apikey=${public_key}&hash=${auth.getHashMd5()}`
+    );
+  },
+};
